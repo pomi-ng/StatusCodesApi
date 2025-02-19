ad927b8596b97d344956e176717a407fcd64eadb
Engjell Rrapaj <engjell.rrapaj@pomiager.com>
2025-02-19 12:14:47 +0100

    fix gitignore

--- obj/Debug/net8.0/StatusCodesApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/StatusCodesApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StatusCodesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db9765be792c38c16828e89a49da1b8755b50be")]
 [assembly: System.Reflection.AssemblyProductAttribute("StatusCodesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StatusCodesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
